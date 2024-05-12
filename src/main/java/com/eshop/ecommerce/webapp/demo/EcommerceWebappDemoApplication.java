e8e64122a46f9df2631ae5b632bcc715a121ba6b
Diwahar Sivaraj <diwahar.sivaraj@sap.com>
2024-05-11 21:09:26 -0500

    initial commit

---
+++ src/main/java/com/eshop/ecommerce/webapp/demo/EcommerceWebappDemoApplication.java
@@ -0,0 +1,13 @@
+package com.eshop.ecommerce.webapp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceWebappDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceWebappDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eshop/ecommerce/webapp/demo/EcommerceWebappDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eshop.ecommerce.webapp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceWebappDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
